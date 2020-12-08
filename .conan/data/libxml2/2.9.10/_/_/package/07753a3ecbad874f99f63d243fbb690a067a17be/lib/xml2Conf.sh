c2c45cb159d440686f21443dfffe38c48888ceb8
Patricia Aas <psmaas@gmail.com>
2020-12-08 09:32:17 +0100

    Local build

---
+++ .conan/data/libxml2/2.9.10/_/_/package/07753a3ecbad874f99f63d243fbb690a067a17be/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/conan/w/cci_PR-3080/.conan/data/libxml2/2.9.10/_/_/package/07753a3ecbad874f99f63d243fbb690a067a17be/lib"
+XML2_LIBS="-lxml2 -L/home/conan/w/cci_PR-3080/.conan/data/zlib/1.2.11/_/_/package/d50a0d523d98c15bb147b18fa7d203887c38be8b/lib -lz -Wl,-rpath=/home/conan/w/cci_PR-3080/.conan/data/zlib/1.2.11/_/_/package/d50a0d523d98c15bb147b18fa7d203887c38be8b/lib    -liconv  -lm "
+XML2_INCLUDEDIR="-I/home/conan/w/cci_PR-3080/.conan/data/libxml2/2.9.10/_/_/package/07753a3ecbad874f99f63d243fbb690a067a17be/include/libxml2"
+MODULE_VERSION="xml2-2.9.10"
+
