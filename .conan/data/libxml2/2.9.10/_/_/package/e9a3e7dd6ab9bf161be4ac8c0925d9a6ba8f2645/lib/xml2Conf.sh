b59562358b31237811a8bb68b0e963f2e40b0a6a
Your Name <your_email_address@example.com>
2020-12-09 06:17:17 +0000

    Local build

---
+++ .conan/data/libxml2/2.9.10/_/_/package/e9a3e7dd6ab9bf161be4ac8c0925d9a6ba8f2645/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/conan/w/cci_PR-3080/.conan/data/libxml2/2.9.10/_/_/package/e9a3e7dd6ab9bf161be4ac8c0925d9a6ba8f2645/lib"
+XML2_LIBS="-lxml2 -L/home/conan/w/cci_PR-3080/.conan/data/zlib/1.2.11/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/lib -lz -Wl,-rpath=/home/conan/w/cci_PR-3080/.conan/data/zlib/1.2.11/_/_/package/6af9cc7cb931c5ad942174fd7838eb655717c709/lib    -liconv  -lm "
+XML2_INCLUDEDIR="-I/home/conan/w/cci_PR-3080/.conan/data/libxml2/2.9.10/_/_/package/e9a3e7dd6ab9bf161be4ac8c0925d9a6ba8f2645/include/libxml2"
+MODULE_VERSION="xml2-2.9.10"
+
